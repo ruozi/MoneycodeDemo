6cfedff6121b2e59519f17881a24bf6a77717c37
Ruozi <wizardfan88@gmail.com>
2015-03-14 11:03:56 +0800

    First
    First Commit

---
+++ gen/com/example/moneycodedemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.moneycodedemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/moneycodedemo/MyApp.java
@@ -0,0 +1,8 @@
+package com.example.moneycodedemo;
+
+
+import android.app.Application;
+
+public class MyApp extends Application{
+
+}
