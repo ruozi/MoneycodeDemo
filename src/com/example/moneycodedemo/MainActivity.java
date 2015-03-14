ba2bc318f8a1b22a5c2bc8449689dc08aa4839fb
Ruozi <wizardfan88@gmail.com>
2015-03-14 11:34:13 +0800

    主界面
    主界面完成

--- src/com/example/moneycodedemo/MainActivity.java
+++ src/com/example/moneycodedemo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.moneycodedemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "Main::Activity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button button_login = (Button)findViewById(R.id.imageButton_login);
+		button_login.setOnClickListener(
+	        	new View.OnClickListener() {			
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Log.d(TAG,"Click!");									
+
+				}
+	        	}
+	        );
 	}
 }
